8620d82512795f85d8572aed00a3714bfc8336fd
nicsaliani <nicolas.saliani@gmail.com>
2024-09-23 17:56:56 -0500

    Update asteroid_manager.gd
    Fixed bug in _on_timer_timeout() causing asteroids to spawn in sizes not included in the list of available sizes.

--- scripts/asteroid_manager.gd
+++ scripts/asteroid_manager.gd
@@ -1,19 +1,27 @@
 class_name AsteroidManager extends Control
 
+## SIGNALS
+## -------------------
+## TODO: Integrate and attach functions to this signal
 signal asteroid_destroyed(asteroid)
 
+## ON-READY REFERENCES
+## -------------------
 @onready var midi_control: MidiControl = $"../MIDI Control"
 @onready var asteroid_spawn_timer: Timer = $"Asteroid Spawn Timer"
 
+## EXPORT REFERNECES
+## -------------------
 @export var asteroid_scene := preload("res://scenes/asteroid.tscn")
 
+## VARIABLES
+## -------------------
 # Vertical/Horizontal boundary values
 const MIN_BOUNDARY = -256
 const MAX_BOUNDARY = 256
 
 var asteroids_on_screen: Dictionary = {}
 var pitches_available: Array[int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-
 var space_available: int = 12
 var spawn_points: Array[Vector2] = [
 	Vector2(MIN_BOUNDARY, MIN_BOUNDARY),
@@ -26,23 +34,21 @@ var spawn_points: Array[Vector2] = [
 	Vector2(MAX_BOUNDARY, MAX_BOUNDARY),
 ]
 
+## FUNCTIONS
+## -------------------
 func _ready():
 	
 	# Connect necessary signals
 	midi_control.connect("note_pressed", _on_note_pressed)
 	asteroid_spawn_timer.connect("timeout", _on_timer_timeout)
 	
-func spawn_asteroid(amount: int, pos: Vector2, size: Asteroid.SizeType):
+func spawn_asteroid(amount: int, pos: Vector2, size: Asteroid.SizeType) -> Asteroid:
 	
 	# Instantiate asteroid and set vars
 	var asteroid = asteroid_scene.instantiate()
 	asteroid.size = size
 	asteroid.pitch = pitches_available[randi_range(0, pitches_available.size()-1)]
 	add_child(asteroid)
-	
-	# Update asteroid-related lists
-	asteroids_on_screen[asteroid.pitch_letter] = asteroid
-	pitches_available.erase(asteroid.pitch)
 	
 	# Position this asteroid so that it spawns just out of sight
 	var asteroid_radius = asteroid.collision_shape_2d.shape.radius
@@ -51,15 +57,12 @@ func spawn_asteroid(amount: int, pos: Vector2, size: Asteroid.SizeType):
 		pos.y + (sign(pos.y)) * asteroid_radius
 	)
 	
-	# Update space available based on this asteroid's size
-	update_space_available(false, asteroid.size)
-	#match asteroid.size:
-		#Asteroid.SizeType.SMALL:
-			#space_available -= 1
-		#Asteroid.SizeType.MEDIUM:
-			#space_available -= 2
-		#Asteroid.SizeType.LARGE:
-			#space_available -= 4
+	# Update asteroid-related lists
+	asteroids_on_screen[asteroid.pitch_letter] = asteroid
+	pitches_available.erase(asteroid.pitch)
+	
+	# Return the new asteroid to be used in other functions
+	return asteroid
 
 func _on_timer_timeout():
 	
@@ -68,13 +71,14 @@ func _on_timer_timeout():
 	
 	# Spawn one new asteroid if possible
 	if pitches_available and sizes_available:
-		spawn_asteroid(
+		var _asteroid = spawn_asteroid(
 			1,
 			spawn_points[randi_range(0, spawn_points.size() - 1)],
-			randi_range(0, Asteroid.SizeType.size() - 1)
+			randi_range(0, sizes_available.size() - 1)
 		)
+		update_space_available(false, _asteroid.size)
 	print(space_available)
-	print(sizes_available)
+	print(check_space_available_for_possible_sizes())
 	
 func _on_note_pressed(played_note: String):
 	if asteroids_on_screen.has(played_note):
@@ -109,6 +113,7 @@ func update_space_available(operation: bool, asteroid_size: Asteroid.SizeType) -
 	else:
 		operation_value = -1
 	
+	# Add or subtract to available_size
 	match asteroid_size:
 		Asteroid.SizeType.SMALL:
 			space_available += 1 * operation_value
