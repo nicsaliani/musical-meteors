73d9709461a63f47cc2b69104735adaab58c6f74
nicsaliani <nicolas.saliani@gmail.com>
2024-12-04 20:06:03 -0600

    Add documentation to game_state_changer.gd
    Add documentation to game_state_changer.gd

--- scripts/game_state_changer.gd
+++ scripts/game_state_changer.gd
@@ -1,18 +1,29 @@
 extends Node
+## Script for handing game state changing.
+##
+## This script is attached to the Main node. It is not an Autoload because
+## Autoloads cannot easily receive signals from other nodes. This script acts
+## as a centralized hub that receives all signals for changing the game state.
 
-## FUNCTIONS
+## ---FUNCTIONS---
+## Set the game state to MAIN_MENU on startup.
 func _ready() -> void:
 	GameManager.set_game_state(GameManager.GameState.MAIN_MENU)
 
+## Set the game state to COUNTDOWN.
 func _on_start_game_button_pressed() -> void:
 	GameManager.set_game_state(GameManager.GameState.COUNTDOWN)
 
+## Set the game state to PLAYING.
 func _on_notif_label_start_game() -> void:
 	GameManager.set_game_state(GameManager.GameState.PLAYING)
 
+## Set the game state to GAME_OVER.
 func _on_game_timer_panel_game_over() -> void:
 	GameManager.set_game_state(GameManager.GameState.GAME_OVER)
 
+## Set the game state to MAIN_MENU when a main menu button is pressed,
+## and unpause the game in case we are coming from the pause menu.
 func _on_main_menu_button_pressed() -> void:
 	GameManager.set_game_state(GameManager.GameState.MAIN_MENU)
 	get_tree().paused = false
